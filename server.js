17d701ad4de27cd4b62244216c53cc81905ef4e7
Baljot Hansi <baljotshansi@gmail.com>
2023-10-07 19:14:45 -0300

    Added logic to display notes.html when the user requests it.

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const app = express();
 
 app.use(express.static('public'));
 
+app.get('/notes', (req, res) =>
+res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
