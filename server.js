5f14a61cc87332ac207042b8285f48b3a6386a8b
Baljot Hansi <baljotshansi@gmail.com>
2023-10-08 11:29:30 -0300

    Added POST functionality to save note. Changed note GET request also.

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const path = require('path');
-const notes = require('./db/db.json');
+const fs = require('fs');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
@@ -12,12 +15,38 @@ app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-app.get('/api/notes', (req, res) =>
-    res.json(notes)
+app.get('/api/notes', (req, res) => 
+    fs.readFile('./db/db.json', (err, data) =>
+            err ?
+            console.log("Error reading file: " + err) :
+            res.json(JSON.parse(data))
+    )
 );
 
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
+
+app.post('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', (err, data) => {
+        if(err) {
+            console.log("Error reading file: " + err);
+        }
+        else {
+            const { title, text } = req.body;
+            notes = JSON.parse(data);
+            notes.push({ title, text });
+
+            fs.writeFile(
+                './db/db.json',
+                JSON.stringify(notes, null, 4),
+                (err) =>
+                    err ?
+                    console.error(err) :
+                    res.json("Note saved successfully.")
+            );
+        }
+    });
+});
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}\nLink: http://localhost:3001/`));
